b8ec15878b178776069693555c27a20b033d1cb6
Matthew Armfield <armfield@airtype.com>
2020-08-11 14:22:46 -0400

    Add in useEffect, useContext, useMemo, useRef hooks Create custom http hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,37 +1,90 @@
-import React, { useState } from "react";
+import React, { useCallback, useReducer, useMemo, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
+import ErrorModal from '../UI/ErrorModal';
+import useHttp from '../../hooks/http';
+
+const ingredientReducer = (currentIngredients, action) => {
+    switch (action.type) {
+        case 'SET':
+            return action.ingredients;
+        case 'ADD':
+            return [...currentIngredients, action.ingredient];
+        case 'DELETE':
+            return currentIngredients.filter(ing => ing.id !== action.id);
+        default:
+            throw new Error('Should not get there!');
+    }
+}
+
 
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
+    const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+    const { 
+        isLoading, 
+        error, 
+        data, 
+        sendRequest, 
+        reqExtra, 
+        reqIdentifier, 
+        clear 
+    } = useHttp();
 
-  const addIngredientHandler = (ingredient) => {
-    fetch("https://matt-react-hooks.firebaseio.com/ingredients.json", {
-      method: "POST",
-      body: JSON.stringify(ingredient),
-      headers: { "Content-Type": "application/json" },
-    }).then((response) => {
-      return response.json();
-    }).then(responseData => {
-      setUserIngredients((prevIngredients) => [
-        ...prevIngredients,
-        { id: responseData.name, ...ingredient },
-      ]);
-    });
-  };
+    const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+        dispatch({ type: 'SET', ingredients: filteredIngredients });
+    }, []);
 
-  return (
-    <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+    useEffect(() => {
+        if (!isLoading && !error && reqIdentifier === 'REMOVE_INGREDIENT') {
+            dispatch({ type: 'DELETE', id: reqExtra })
+        } else if (!isLoading && !error && reqIdentifier === 'ADD_INGREDIENT') {
+            dispatch({
+                type: 'ADD',
+                ingredient: { id: data.name, ...reqExtra }
+            })
+        }
+    }, [data, reqExtra, reqIdentifier, isLoading]);
 
-      <section>
-        <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
-      </section>
-    </div>
-  );
+
+    const addIngredientHandler = useCallback((ingredient) => {
+        sendRequest(
+            'https://matt-react-hooks.firebaseio.com/ingredients.json',
+            'POST',
+            JSON.stringify(ingredient),
+            ingredient,
+            'ADD_INGREDIENT'
+        );
+    }, [sendRequest]);
+
+    const removeIngredientHandler = useCallback(ingredientId => {
+        sendRequest(
+            `https://matt-react-hooks.firebaseio.com/ingredients/${ingredientId}.json`,
+            'DELETE',
+            null,
+            ingredientId,
+            'REMOVE_INGREDIENT'
+        );
+    }, [sendRequest]);
+
+    const ingredientList = useMemo(() => {
+        return (
+            <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
+        );
+    }, [userIngredients, removeIngredientHandler]);
+
+    return (
+        <div className="App">
+            {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
+            <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
+
+            <section>
+                <Search onLoadIngredients={filteredIngredientsHandler} />
+                {ingredientList}
+            </section>
+        </div>
+    );
 };
 
 export default Ingredients;
